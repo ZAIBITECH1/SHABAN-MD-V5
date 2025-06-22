8e4d19d5bc3e1ca3ffc7b69bedf5dcd79eed9706
ZAIBITECH1 <127785281+ZAIBITECH1@users.noreply.github.com>
2025-06-22 21:46:13 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "SHABAN-MD~7wgwgKZb#k6suJcO17bI1ZfvCqMx5k3Cz10veTWGOq-f6bd1Ir5k",
 // add your Session Id
 AUTO_BIO: process.env.AUTO_BIO || "false", 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
